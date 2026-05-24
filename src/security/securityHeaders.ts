a2063c96f292ce1cc1e9373f8c156e6fdb5b8812
github.com/Kaif-ansari <ansarisaif9548@gmail.com>
2026-05-25 00:34:45 +0530

    update project

--- src/security/securityHeaders.ts
+++ src/security/securityHeaders.ts
@@ -1,12 +1,12 @@
 export const securityHeaders: Record<string, string> = {
   "Content-Security-Policy": [
     "default-src 'self'",
-    "script-src 'self' 'unsafe-inline' https://cdn.skypack.dev https://cdnjs.cloudflare.com https://www.googletagmanager.com",
+    "script-src 'self' 'unsafe-inline' https://cdn.skypack.dev https://cdnjs.cloudflare.com https://www.googletagmanager.com https://tagmanager.google.com https://www.google-analytics.com",
     "worker-src 'self' blob: https://cdnjs.cloudflare.com",
     "frame-src 'self' blob: https://www.googletagmanager.com",
-    "connect-src 'self' https://www.googletagmanager.com",
-    "img-src 'self' data: blob: https://www.googletagmanager.com",
-    "style-src 'self' 'unsafe-inline'",
+    "connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://region1.google-analytics.com https://stats.g.doubleclick.net",
+    "img-src 'self' data: blob: https://www.googletagmanager.com https://www.google-analytics.com https://ssl.gstatic.com https://www.gstatic.com",
+    "style-src 'self' 'unsafe-inline' https://tagmanager.google.com",
     "font-src 'self'",
     "object-src 'none'",
     "base-uri 'self'",
